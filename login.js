1542e46385f67216d8da15dc05cd1207a6324499
MySuperHer <103127208+MySuperHer@users.noreply.github.com>
2022-04-08 19:42:32 +0800

    Delete login.js

--- login.js
+++
@@ -1,7 +0,0 @@
-function gohome(){
-    window.location.href='https://mysuperher.github.io/MySuperMen/index.html';
-    var h=document.getElementById("user_id");
-    localStorage.setItem("name",h);
-    var h2=document.getElementById("user_password");
-    localStorage.setItem("bg",h2);
-}
