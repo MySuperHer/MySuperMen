023ba6ca23dc08f114767255eec4d449196ae12d
MySuperHer <iamkillwould@gmail.com>
2022-04-11 00:57:45 +0800

    my

--- lvup.js
+++ lvup.js
@@ -20,8 +20,9 @@ function goback(){
         localStorage.setItem("m11","31");
         localStorage.setItem("age","0");
         localStorage.setItem("do","0");
-        localStorage.setItem("goback","0");
         localStorage.setItem("g01","0");
+        localStorage.setItem("goback","0");
+       
     }
 }
 
@@ -48,15 +49,15 @@ async function gohigh(){
     $('#train3').attr('disabled', true);
     $('#train4').attr('disabled', true);
     $('#train5').attr('disabled', true);
-    await delay(0);
+    await delay(20);
     var g11=Number(localStorage.getItem("g11"))+3;
-    localStorage.setItem("g11",String(g11));
+    localStorage.setItem("g11",g11.toString());
     var j11=Number(localStorage.getItem("j11"))+1;
-    localStorage.setItem("j11",String(j11));
+    localStorage.setItem("j11",j11.toString());
     var g21=Number(localStorage.getItem("g21"))-3;
-    localStorage.setItem("g21",String(g21));
+    localStorage.setItem("g21",g21.toString());
     var g31=Number(localStorage.getItem("g31"))-1;
-    localStorage.setItem("g31",String(g31));
+    localStorage.setItem("g31",g31.toString());
     g21correction();
     g31correction();
     gobig();
@@ -75,13 +76,13 @@ async function gofat(){
     $('#train5').attr('disabled', true);
     await delay(20);
     var g31=Number(localStorage.getItem("g31"))+9;
-    localStorage.setItem("g11",String(g11));
+    localStorage.setItem("g11",g11.toString());
     var i11=Number(localStorage.getItem("i11"))-5;
-    localStorage.setItem("i11",String(i11));
+    localStorage.setItem("i11",i11.toString());
     var g11=Number(localStorage.getItem("g11"))-3;
-    localStorage.setItem("g11",String(g11));
+    localStorage.setItem("g11",g11.toString());
     var g21=Number(localStorage.getItem("g21"))-1;
-    localStorage.setItem("g21",String(g21));
+    localStorage.setItem("g21",g21.toString());
     g11correction();
     g21correction();
     i11correction();
@@ -101,15 +102,15 @@ async function backfat(){
     $('#train5').attr('disabled', true);
     await delay(20);
     var j11=Number(localStorage.getItem("j11"))+4;
-    localStorage.setItem("j11",String(j11));
+    localStorage.setItem("j11",j11.toString());
     var g21=Number(localStorage.getItem("g21"))+3;
-    localStorage.setItem("g21",String(g21));
+    localStorage.setItem("g21",g21.toString());
     var l11=Number(localStorage.getItem("l11"))+1;
-    localStorage.setItem("l11",String(l11));
+    localStorage.setItem("l11",l11.toString());
     var g31=Number(localStorage.getItem("g31"))-7;
-    localStorage.setItem("g31",String(g31));
+    localStorage.setItem("g31",g31.toString());
     var i11=Number(localStorage.getItem("i11"))-1;
-    localStorage.setItem("i11",String(i11));
+    localStorage.setItem("i11",i11.toString());
     g31correction();
     i11correction();
     $("p").append("<div>到底有誰會想吃這鬼東西</div>");
@@ -128,15 +129,15 @@ async function badeye(){
     $('#train5').attr('disabled', true);
     await delay(20);
     var i11=Number(localStorage.getItem("i11"))+4;
-    localStorage.setItem("i11",String(i11));
+    localStorage.setItem("i11",i11.toString());
     var g31=Number(localStorage.getItem("g31"))*1.1;
-    localStorage.setItem("g31",String(g31));
+    localStorage.setItem("g31",g31.toString());
     var g21=Number(localStorage.getItem("g21"))*0.8;
-    localStorage.setItem("g21",String(g21));
+    localStorage.setItem("g21",g21.toString());
     var l11=Number(localStorage.getItem("l11"))-7;
-    localStorage.setItem("l11",String(l11));
+    localStorage.setItem("l11",l11.toString());
     var j11=Number(localStorage.getItem("j11"))-1;
-    localStorage.setItem("j11",String(j11));
+    localStorage.setItem("j11",j11.toString());
     j11correction();
     l11correction();
     $("p").append("<div>刀劍神域才不是卡通</div>");
@@ -157,13 +158,13 @@ async function useeye(){
     var a1 = Math.random()*8;
     var b1 = ["g11", "g21", "g31", "j11", "l11", "l21", "i11", "m11"];
     var r1=Number(localStorage.getItem(b1[a1]))*1.1;
-    localStorage.setItem(b1[a1],String(r1));
+    localStorage.setItem(b1[a1],r1.toString());
     var b2 = b.splice(pos, a1);
     var a2 = Math.random()*7;
     var r2=Number(localStorage.getItem(b2[a2]))*0.09;
-    localStorage.setItem(b2[a2],String(r2));
+    localStorage.setItem(b2[a2],r2.toString());
     var l21=Number(localStorage.getItem("l21"))+3;
-    localStorage.setItem("l21",String(l21));
+    localStorage.setItem("l21",l21.toString());
     l11correction();
     $("p").append("<div>看三小還想星爆啊</div>");
     gobig();
@@ -178,24 +179,42 @@ function gobig(){
     var age=Number(localStorage.getItem("age"))+1;
     var age2=age*2;
     if (do1 == age2){
-        var g0=(Number(localStorage.getItem(g01)));
-        var g11=parseInt(Number(localStorage.getItem(g11))/5);
-        var g21=parseInt(Number(localStorage.getItem(g21))/5);
-        var g31=parseInt(Number(localStorage.getItem(g31))/5);
-        var j11=parseInt(Number(localStorage.getItem(j11))/5);
-        var l11=parseInt(Number(localStorage.getItem(l11))/5);
-        var l21=parseInt(Number(localStorage.getItem(l21))/5);
-        var i11=parseInt(Number(localStorage.getItem(i11))/5);
-        var m11=parseInt(Number(localStorage.getItem(m11))/5);
-        var g1=Number(localStorage.getItem(g1))+(g11)*(1+g0/20)/24;
-        var g2=Number(localStorage.getItem(g2))+(g21)*(1+g0/20)/6;
-        var g3=Number(localStorage.getItem(g3))+(g31)*(1+g0/20)/6;
-        var j1=Number(localStorage.getItem(j1))+(j11)*(1+g0/20)/5;
-        var l1=Number(localStorage.getItem(l1))+(l11)*(1+g0/20)/5;
-        var l2=Number(localStorage.getItem(l2))+(l21)*(1+g0/20)/5;
-        var i1=Number(localStorage.getItem(i1))+(i11)*(1+g0/20)/5;
-        var m1=Number(localStorage.getItem(m1))+(m11)*(1+g0/20)/5;
-        var hp=Number(localStorage.getItem(hp))+10*(1+g0/20);
+        var g0=Number(localStorage.getItem("g01"));
+        var g11=parseInt(Number(localStorage.getItem("g11"))/5);
+        var g21=parseInt(Number(localStorage.getItem("g21"))/5);
+        var g31=parseInt(Number(localStorage.getItem("g31"))/5);
+        var j11=parseInt(Number(localStorage.getItem("j11")))/5;
+        var l11=parseInt(Number(localStorage.getItem("l11")))/5;
+        var l21=parseInt(Number(localStorage.getItem("l21")))/5;
+        var i11=parseInt(Number(localStorage.getItem("i11")))/5;
+        var m11=parseInt(Number(localStorage.getItem("m11")))/5;
+        var g1x=g11*(1+g0/20)/24;
+        var g2x=g21*(1+g0/20)/6;
+        var g3x=g31*(1+g0/20)/6;
+        var j1x=j11*(1+g0/20)/5;
+        var l1x=l11*(1+g0/20)/5;
+        var l2x=l21*(1+g0/20)/5;
+        var i1x=i11*(1+g0/20)/5;
+        var m1x=m11*(1+g0/20)/5;
+        var hpx=10*(1+g0/20);
+        var cg1=Number(localStorage.getItem("g1"));
+        var cg2=Number(localStorage.getItem("g2"));
+        var cg3=Number(localStorage.getItem("g3"));
+        var cj1=Number(localStorage.getItem("j1"));
+        var cl1=Number(localStorage.getItem("l1"));
+        var cl2=Number(localStorage.getItem("l2"));
+        var ci1=Number(localStorage.getItem("i1"));
+        var cm1=Number(localStorage.getItem("m1"));
+        var chp=Number(localStorage.getItem("hp"));
+        var g1=cg1+g1x;
+        var g2=cg2+g2x;
+        var g3=cg3+g3x;
+        var j1=cj1+j1x;
+        var l1=cl1+l1x;
+        var l2=cl2+l2x;
+        var i1=ci1+i1x;
+        var m1=cm1+m1x;
+        var hp=chp+hpx;
         if (g1>20*(1+g0/100)){
             g1=20*(1+g0/20);
             if(age>50){
@@ -224,27 +243,35 @@ function gobig(){
                 i1=i1*(1-0.002*age);
             }
         }
-        localStorage.setItem("hp",String(hp));
-        localStorage.setItem("g1",String(g1));
+        localStorage.setItem("hp",hp.toString());
+        localStorage.setItem("g1",g1.toString());
         localStorage.setItem("g11","0");
-        localStorage.setItem("g2",String(g2));
+        localStorage.setItem("g2",g2.toString());
         localStorage.setItem("g21","0");
-        localStorage.setItem("g3",String(g3));
+        localStorage.setItem("g3",g3.toString());
         localStorage.setItem("g31","0");
-        localStorage.setItem("j1",String(j1));
+        localStorage.setItem("j1",j1.toString());
         localStorage.setItem("j11","0");
-        localStorage.setItem("l1",String(l1));
+        localStorage.setItem("l1",l1.toString());
         localStorage.setItem("l11","0");
-        localStorage.setItem("l2",String(l2));
+        localStorage.setItem("l2",l2.toString());
         localStorage.setItem("l21","0");
-        localStorage.setItem("i1",String(i1));
+        localStorage.setItem("i1",i1.toString());
         localStorage.setItem("i11","0");
-        localStorage.setItem("m1",String(m1));
+        localStorage.setItem("m1",m1.toString());
         localStorage.setItem("m11","0");
-        localStorage.setItem("age",String(age));
-        $("p").append("<div>你長大了，成果如下\n身高成長了"+String((g11)*(1+g0/20)/24)+"cm\n體重成長了"+String(((g21)*(1+g0/20)/3+(g11)*(1+g0/20)/24))+"kg\n跳躍力成長了"+String((j11)*(1+g0/20)/5)+"\n視力成長了"+String((l11)*(1+g0/20)/5))+"\n幸運成長了"+String((l21)*(1+g0/20)/5)+"\n智力成長了"+String((i11)*(1+g0/20)/5)</div>;
+        localStorage.setItem("age",age.toString());
+        let content = '';
+        content += '<div>你長大了，成果如下</div>';
+        content += '<div>身高成長了' + String(parseInt(g1)-parseInt(cg1)) + 'cm</div>';
+        content += '<div>體重成長了' + String(parseInt(g1+g2+g3)-parseInt(cg1-cg2-cg3)) + 'kg</div>';
+        content += '<div>跳躍力成長了' + String(parseInt(j1)-parseInt(cj1)) + '</div>';
+        content += '<div>視力成長了' + String(parseInt(l1)-parseInt(cl1)) + '</div>';
+        content += '<div>幸運成長了' + String(parseInt(l2)-parseInt(cl2)) + '</div>';
+        content += '<div>智力成長了' + String(parseInt(i1)-parseInt(ci1)) + '</div>';
+        $("p").append(content);
     } else {
-        localStorage.setItem("do",String(do1));
+        localStorage.setItem("do",do1.toString());
     }
 }
 
