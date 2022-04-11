16458ebb05b110cb3d8b9874b703485a3d3543f6
MySuperHer <iamkillwould@gmail.com>
2022-04-11 20:48:53 +0800

    hi2

--- lvup.js
+++ lvup.js
@@ -528,7 +528,7 @@ function win(){
     localStorage.setItem("l21",l21.toString());
     m11correction();
     l21correction();
-    gobig();
+    gobig2();
 }
 
 function lose(){
@@ -541,5 +541,180 @@ function lose(){
     var i11=Number(localStorage.getItem("i11"))-9;
     localStorage.setItem("i11",i11.toString());
     i11correction();
-    gobig();
+    gobig2();
+}
+
+function gobig2(){
+    var do1=Number(localStorage.getItem("do"))+1;
+    var age=Number(localStorage.getItem("age"))+1;
+    if(age>=35){
+        var age2=age;
+    }else if(age>=20){
+        var age2=age+20;
+    }else{
+        var age2=age*2;
+    }
+    var work=localStorage.getItem("work");
+    if (work == "跳級生"){
+        var do1=Number(localStorage.getItem("do"))+1;
+        var i11=Number(localStorage.getItem("i11"))+5;
+        var g31=Number(localStorage.getItem("g31"))-2;
+        localStorage.setItem("i11",i11.toString());
+        localStorage.setItem("g31",g31.toString());
+        g31correction();
+        if(age == 30){
+            var i1=Number(localStorage.getItem("i1"));
+            if(i1<50){
+                localStorage.setItem("work","延畢生");
+            }else if(i1<90){
+                localStorage.setItem("work","畢業生");
+            }else{
+                localStorage.setItem("work","大學教授");
+            }
+        }
+    }else if(work == "研究生"){
+        var do1=Number(localStorage.getItem("do"))+1;
+        var i11=Number(localStorage.getItem("i11"))+3;
+        var g31=Number(localStorage.getItem("g31"))-4;
+        localStorage.setItem("i11",i11.toString());
+        localStorage.setItem("g31",g31.toString());
+        g31correction();
+        if(age == 30){
+            var i1=Number(localStorage.getItem("i1"));
+            if(i1<50){
+                localStorage.setItem("work","延畢生");
+            }else{
+                localStorage.setItem("work","畢業生");
+            }
+        }
+    }else if(work == "延畢生"){
+        var do1=Number(localStorage.getItem("do"))+1;
+        var i11=Number(localStorage.getItem("i11"))+1;
+        var g31=Number(localStorage.getItem("g31"))-5;
+        localStorage.setItem("i11",i11.toString());
+        localStorage.setItem("g31",g31.toString());
+        g31correction();
+        if(age == 30){
+            var i1=Number(localStorage.getItem("i1"));
+            if(i1>50){
+                localStorage.setItem("work","畢業生");
+            }
+        }
+    }else if(work == "畢業生"){
+        var do1=Number(localStorage.getItem("do"))+1;
+        var i11=Number(localStorage.getItem("i11"))+4;
+        var g31=Number(localStorage.getItem("g31"))-2;
+        localStorage.setItem("i11",i11.toString());
+        localStorage.setItem("g31",g31.toString());
+        g31correction();
+    }else if(work == "大學教授"){
+        var do1=Number(localStorage.getItem("do"))+1;
+        var i11=Number(localStorage.getItem("i11"))*2;
+        var g31=Number(localStorage.getItem("g31"))/2;
+        localStorage.setItem("i11",i11.toString());
+        localStorage.setItem("g31",g31.toString());
+    }
+    if (do1 == age2){
+        var g0=Number(localStorage.getItem("g01"));
+        var g11=parseInt(Number(localStorage.getItem("g11"))/5);
+        var g21=parseInt(Number(localStorage.getItem("g21"))/5);
+        var g31=parseInt(Number(localStorage.getItem("g31"))/5);
+        var j11=parseInt(Number(localStorage.getItem("j11")))/5;
+        var l11=parseInt(Number(localStorage.getItem("l11")))/5;
+        var l21=parseInt(Number(localStorage.getItem("l21")))/5;
+        var i11=parseInt(Number(localStorage.getItem("i11")))/5;
+        var m11=parseInt(Number(localStorage.getItem("m11")))/5;
+        var g1x=g11*(1+g0/20)/19.5;
+        var g2x=g21*(1+g0/20)/3.8;
+        var g3x=g31*(1+g0/20)/3.6;
+        var j1x=j11*(1+g0/20)/1.5;
+        var l1x=l11*(1+g0/20)/2.75;
+        var l2x=l21*(1+g0/20)/3.5;
+        var i1x=i11*(1+g0/20)/3.25;
+        var m1x=m11*(1+g0/20)/3.1;
+        var hpx=10*(1+g0/20);
+        var cg1=Number(localStorage.getItem("g1"));
+        var cg2=Number(localStorage.getItem("g2"));
+        var cg3=Number(localStorage.getItem("g3"));
+        var cj1=Number(localStorage.getItem("j1"));
+        var cl1=Number(localStorage.getItem("l1"));
+        var cl2=Number(localStorage.getItem("l2"));
+        var ci1=Number(localStorage.getItem("i1"));
+        var cm1=Number(localStorage.getItem("m1"));
+        var chp=Number(localStorage.getItem("hp"));
+        var g1=cg1+g1x;
+        var g2=cg2+g2x;
+        var g3=cg3+g3x;
+        var j1=cj1+j1x;
+        var l1=cl1+l1x;
+        var l2=cl2+l2x;
+        var i1=ci1+i1x;
+        var m1=cm1+m1x;
+        var hp=chp+hpx;
+        if (g1>20*(1+g0/100)){
+            g1=20*(1+g0/20);
+            if(age>50){
+                g1=g1*0.99;
+            }
+        }
+        if (g2+g3>g1*14){
+            g2=g2*0.95;
+            g3=g3*0.9;
+        }
+        if (j1>g2*3.5){
+            j1=g2*3.5;
+        }
+        if (l1>100*(1+g0/20)){
+            l1=100;
+            if(age>35){
+                l1=l1*(1-0.005*age);
+            }
+        }
+        if (l2>100*(1+g0/20)){
+            l2=100*(1+g0/20);
+        }
+        if (i1>180*(1+g0/20)){
+            i1=180*(1+g0/20);
+            if (age>30){
+                i1=i1*(1-0.002*age);
+            }
+        }
+        if (m1>100*(1+g0/20)){
+            m1=180*(1+g0/20);
+            if (age>30){
+                m1=m1*(1-0.01*age);
+            }
+        }
+        localStorage.setItem("hp",hp.toString());
+        localStorage.setItem("g1",g1.toString());
+        localStorage.setItem("g11","31");
+        localStorage.setItem("g2",g2.toString());
+        localStorage.setItem("g21","31");
+        localStorage.setItem("g3",g3.toString());
+        localStorage.setItem("g31","31");
+        localStorage.setItem("j1",j1.toString());
+        localStorage.setItem("j11","31");
+        localStorage.setItem("l1",l1.toString());
+        localStorage.setItem("l11","31");
+        localStorage.setItem("l2",l2.toString());
+        localStorage.setItem("l21","31");
+        localStorage.setItem("i1",i1.toString());
+        localStorage.setItem("i11","31");
+        localStorage.setItem("m1",m1.toString());
+        localStorage.setItem("m11","31");
+        localStorage.setItem("do","0");
+        localStorage.setItem("age",age.toString());
+        let content = '';
+        content += '<div>你長大了，成果如下</div>';
+        content += '<div>身高成長了' + String((parseInt(g1*1000)-parseInt(cg1*1000))/100)+ 'cm</div>';
+        content += '<div>體重成長了' + String((parseInt((g1+g2+g3)*100)-parseInt((cg1+cg2+cg3)*100))/100) + 'kg</div>';
+        content += '<div>跳躍力成長了' + String((parseInt(j1*100)-parseInt(cj1*100))/100) + '</div>';
+        content += '<div>視力成長了' + String((parseInt(l1*100)-parseInt(cl1*100))/100) + '</div>';
+        content += '<div>幸運成長了' + String((parseInt(l2*100)-parseInt(cl2*100))/100) + '</div>';
+        content += '<div>智力成長了' + String((parseInt(i1*100)-parseInt(ci1*100))/100) + '</div>';
+        content += '<div>魅力成長了' + String((parseInt(m1*100)-parseInt(cm1*100))/100) + '</div>';
+        $("#3").append(content);
+    } else {
+        localStorage.setItem("do",do1.toString());
+    }
 }
