d92421e61e817b5515e2d65e6b3c049d8a5f666e
MySuperHer <iamkillwould@gmail.com>
2022-04-12 12:26:38 +0800

    i0923

--- lvup.js
+++ lvup.js
@@ -58,8 +58,8 @@ async function gohigh(){
     var j11=Number(localStorage.getItem("j11"))+1;
     var g21=Number(localStorage.getItem("g21"))-3;
     var g31=Number(localStorage.getItem("g31"))-1;
-    gobig(g11,g21,g31,j11,-1,-1,-1,-1);
     $("#1").append("<div>長長的高丸嘗起來真美味。</div>");
+    gobig(g11,g21,g31,j11,-1,-1,-1,-1);
     $('#train1').attr('disabled', false);
     $('#train2').attr('disabled', false);
     $('#train3').attr('disabled', false);
@@ -195,6 +195,14 @@ async function gopretty(){
 }
 
 function gobig(g11,g21,g31,j11,l11,l21,i11,m11){
+    g11correction();
+    g21correction();
+    g31correction();
+    j11correction();
+    l11correction();
+    l21correction();
+    i11correction();
+    m11correction();
     var do1=Number(localStorage.getItem("do"))+1;
     var age=Number(localStorage.getItem("age"));
     var age1=Number(localStorage.getItem("age"))+1;
@@ -203,14 +211,14 @@ function gobig(g11,g21,g31,j11,l11,l21,i11,m11){
     }else if(age>=20){
         var line=age+20;
     }else{
-        var line=age*2;
+        var line=age*2+2;
     }
     if(age1>=35){
         var line1=age1;
     }else if(age1>=20){
         var line1=age1+20;
     }else{
-        var line1=age1*2;
+        var line1=age1*2+2;
     }
     if (do1 >= line){
         g01=Number(localStorage.getItem("g01"));
@@ -304,7 +312,7 @@ function gobig(g11,g21,g31,j11,l11,l21,i11,m11){
         localStorage.setItem("m1",m1.toString());
         localStorage.setItem("m11","31");
         localStorage.setItem("do","0");
-        localStorage.setItem("age",age.toString());
+        localStorage.setItem("age",age1.toString());
         localStorage.setItem("exp",String(line1));
         let content = '';
         content += '<div>你長大了，成果如下</div>';
@@ -347,20 +355,24 @@ function gobig(g11,g21,g31,j11,l11,l21,i11,m11){
 
 
 function work1(work1){
+    var age=Number(localStorage.getItem("age"));
     if (work1 == "跳級生"){
         do1=do1+1;
         i11=i11+5;
         g31=g31-2;
-        if(age == 30){
+        if(age == 27){
             var i1=Number(localStorage.getItem("i1"));
-            if(i1<50){
+            if(i1<70){
                 localStorage.setItem("work","延畢生");
-            }else if(i1<90){
+            }else if(i1<100){
                 localStorage.setItem("work","畢業生");
             }else{
                 localStorage.setItem("work","大學教授");
             }
         }
+        let content = '';
+        content += '<div>你寫了一篇論文</div>';
+        $("#1").append(content);
     }else if(work1 == "研究生"){
         var do1=Number(localStorage.getItem("do"))+1;
         var i11=Number(localStorage.getItem("i11"))+3;
@@ -369,14 +381,17 @@ function work1(work1){
         localStorage.setItem("g31",g31.toString());
         localStorage.setItem("do",do1.toString());
         g31correction();
-        if(age == 30){
+        if(age == 27){
             var i1=Number(localStorage.getItem("i1"));
-            if(i1<50){
+            if(i1<70){
                 localStorage.setItem("work","延畢生");
             }else{
                 localStorage.setItem("work","畢業生");
             }
         }
+        let content = '';
+        content += '<div>你做了一篇研究</div>';
+        $("#1").append(content);
     }else if(work1 == "延畢生"){
         var do1=Number(localStorage.getItem("do"))+1;
         var i11=Number(localStorage.getItem("i11"))+1;
@@ -385,11 +400,19 @@ function work1(work1){
         localStorage.setItem("g31",g31.toString());
         localStorage.setItem("do",do1.toString());
         g31correction();
-        if(age == 30){
+        if(age >= 27){
             var i1=Number(localStorage.getItem("i1"));
-            if(i1>50){
+            if(i1>70){
                 localStorage.setItem("work","畢業生");
+            }else{
+                let content = '';
+                content += '<div>你寫了一篇論文，但沒有通過考試</div>';
+                $("#1").append(content);
             }
+        }else{
+            let content = '';
+            content += '<div>你寫了一篇論文</div>';
+            $("#1").append(content);
         }
     }else if(work1 == "畢業生"){
         var do1=Number(localStorage.getItem("do"))+1;
@@ -399,6 +422,9 @@ function work1(work1){
         localStorage.setItem("g31",g31.toString());
         localStorage.setItem("do",do1.toString());
         g31correction();
+        let content = '';
+        content += '<div>你還沒找到工作</div>';
+        $("#1").append(content);
     }else if(work1 == "大學教授"){
         var do1=Number(localStorage.getItem("do"))+1;
         var i11=Number(localStorage.getItem("i11"))*2;
@@ -406,6 +432,9 @@ function work1(work1){
         localStorage.setItem("i11",i11.toString());
         localStorage.setItem("g31",g31.toString());
         localStorage.setItem("do",do1.toString());
+        let content = '';
+        content += '<div>你在大學教書</div>';
+        $("#1").append(content);
     }
 }
 
@@ -534,176 +563,98 @@ function delay(n){
 
 function win(){
     var g21=Number(localStorage.getItem("g21"))+9;
-    localStorage.setItem("g21",g21.toString());
     var i11=Number(localStorage.getItem("i11"))+5;
-    localStorage.setItem("i11",i11.toString());
     var m11=Number(localStorage.getItem("m11"))-7;
-    localStorage.setItem("m11",m11.toString());
     var l21=Number(localStorage.getItem("l21"))-4;
-    localStorage.setItem("l21",l21.toString());
-    m11correction();
-    l21correction();
-    gobig2();
+    gobig2(-1,g21,-1,-1,-1,l21,i11,m11);
 }
 
 function lose(){
     var g31=Number(localStorage.getItem("g31"))+4;
-    localStorage.setItem("g31",g31.toString());
     var j11=Number(localStorage.getItem("j11"))+3;
-    localStorage.setItem("j11",j11.toString());
     var l11=Number(localStorage.getItem("l11"))+3;
-    localStorage.setItem("l11",l11.toString());
     var i11=Number(localStorage.getItem("i11"))-9;
-    localStorage.setItem("i11",i11.toString());
-    i11correction();
-    gobig2();
+    gobig2(-1,-1,g31,j11,l11,-1,i11,-1);
 }
 
-function gobig2(){
+function gobig2(g11,g21,g31,j11,l11,l21,i11,m11){
+    g11correction();
+    g21correction();
+    g31correction();
+    j11correction();
+    l11correction();
+    l21correction();
+    i11correction();
+    m11correction();
     var do1=Number(localStorage.getItem("do"))+1;
-    var age=Number(localStorage.getItem("age"))+1;
-    var age1=Number(localStorage.getItem("age"))+2;
+    var age=Number(localStorage.getItem("age"));
+    var age1=Number(localStorage.getItem("age"))+1;
     if(age>=35){
-        var age2=age;
+        var line=age;
     }else if(age>=20){
-        var age2=age+20;
+        var line=age+20;
     }else{
-        var age2=age*2;
+        var line=age*2+2;
     }
     if(age1>=35){
-        var age3=age1;
+        var line1=age1;
     }else if(age1>=20){
-        var age3=age1+20;
+        var line1=age1+20;
     }else{
-        var age3=age1*2;
+        var line1=age1*2+2;
     }
-    var work=localStorage.getItem("work");
-    if (work == "跳級生"){
-        var do1=Number(localStorage.getItem("do"))+1;
-        var i11=Number(localStorage.getItem("i11"))+5;
-        var g31=Number(localStorage.getItem("g31"))-2;
-        localStorage.setItem("do",do1.toString());
-        localStorage.setItem("i11",i11.toString());
-        localStorage.setItem("g31",g31.toString());
-        g31correction();
-        if(age == 30){
-            var i1=Number(localStorage.getItem("i1"));
-            if(i1<50){
-                localStorage.setItem("work","延畢生");
-            }else if(i1<90){
-                localStorage.setItem("work","畢業生");
-            }else{
-                localStorage.setItem("work","大學教授");
-            }
-        }
-    }else if(work == "研究生"){
-        var do1=Number(localStorage.getItem("do"))+1;
-        var i11=Number(localStorage.getItem("i11"))+3;
-        var g31=Number(localStorage.getItem("g31"))-4;
-        localStorage.setItem("do",do1.toString());
-        localStorage.setItem("i11",i11.toString());
-        localStorage.setItem("g31",g31.toString());
-        g31correction();
-        if(age == 30){
-            var i1=Number(localStorage.getItem("i1"));
-            if(i1<50){
-                localStorage.setItem("work","延畢生");
-            }else{
-                localStorage.setItem("work","畢業生");
-            }
-        }
-    }else if(work == "延畢生"){
-        var do1=Number(localStorage.getItem("do"))+1;
-        var i11=Number(localStorage.getItem("i11"))+1;
-        var g31=Number(localStorage.getItem("g31"))-5;
-        localStorage.setItem("i11",i11.toString());
-        localStorage.setItem("g31",g31.toString());
-        localStorage.setItem("do",do1.toString());
-        g31correction();
-        if(age == 30){
-            var i1=Number(localStorage.getItem("i1"));
-            if(i1>50){
-                localStorage.setItem("work","畢業生");
-            }
-        }
-    }else if(work == "畢業生"){
-        var do1=Number(localStorage.getItem("do"))+1;
-        var i11=Number(localStorage.getItem("i11"))+4;
-        var g31=Number(localStorage.getItem("g31"))-2;
-        localStorage.setItem("i11",i11.toString());
-        localStorage.setItem("g31",g31.toString());
-        localStorage.setItem("do",do1.toString());
-        g31correction();
-    }else if(work == "大學教授"){
-        var do1=Number(localStorage.getItem("do"))+1;
-        var i11=Number(localStorage.getItem("i11"))*2;
-        var g31=Number(localStorage.getItem("g31"))/2;
-        localStorage.setItem("i11",i11.toString());
-        localStorage.setItem("g31",g31.toString());
-        localStorage.setItem("do",do1.toString());
-    }
-    if (do1 == age2){
-        var g01=Number(localStorage.getItem("g01"));
-        var g02=Number(localStorage.getItem("g02"));
-        var g03=Number(localStorage.getItem("g03"));
-        var j01=Number(localStorage.getItem("j01"));
-        var l01=Number(localStorage.getItem("l01"));
-        var l02=Number(localStorage.getItem("l02"));
-        var i01=Number(localStorage.getItem("i01"));
-        var m01=Number(localStorage.getItem("m01"));
-        var g11=parseInt(Number(localStorage.getItem("g11"))/5);
-        var g21=parseInt(Number(localStorage.getItem("g21"))/5);
-        var g31=parseInt(Number(localStorage.getItem("g31"))/5);
-        var j11=parseInt(Number(localStorage.getItem("j11"))/5);
-        var l11=parseInt(Number(localStorage.getItem("l11"))/5);
-        var l21=parseInt(Number(localStorage.getItem("l21"))/5);
-        var i11=parseInt(Number(localStorage.getItem("i11"))/5);
-        var m11=parseInt(Number(localStorage.getItem("m11"))/5);
-        var g1x=g11*(0.2+0.1*g01/(g01+1));
-        var g2x=g21*(0.3+0.3*Math.sqrt(g01)*g02/((Math.sqrt(g01)+1)*(g02+1)));
-        var g3x=g31*(0.3+0.3*Math.sqrt(g01)*g03/((Math.sqrt(g01)+1)*(g03+1)));
-        var j1x=j11*(0.2+0.5*Math.sqrt(g01)*j01/((Math.sqrt(g01)+1)*(j01+1)));
-        var l1x=l11*(0.3+0.3*Math.sqrt(g01)*l01/((Math.sqrt(g01)+1)*(l01+1)));
-        var l2x=l21*(0.2+0.2*Math.sqrt(g01)*l02/((Math.sqrt(g01)+1)*(l02+1)));
-        var i1x=i11*(0.1+0.1*Math.sqrt(g01)*i01/((Math.sqrt(g01)+1)*(i01+1)));
-        var m1x=m11*(0.3+0.4*Math.sqrt(g01)*m01/((Math.sqrt(g01)+1)*(m01+1)));
-        var hpx=10*(0.1+0.1*g01/(g01+1));
-        var cg1=Number(localStorage.getItem("g1"));
-        var cg2=Number(localStorage.getItem("g2"));
-        var cg3=Number(localStorage.getItem("g3"));
-        var cj1=Number(localStorage.getItem("j1"));
-        var cl1=Number(localStorage.getItem("l1"));
-        var cl2=Number(localStorage.getItem("l2"));
-        var ci1=Number(localStorage.getItem("i1"));
-        var cm1=Number(localStorage.getItem("m1"));
-        var chp=Number(localStorage.getItem("hp"));
-        var g1=cg1+g1x;
-        var g2=cg2+g2x;
-        var g3=cg3+g3x;
-        var j1=cj1+j1x;
-        var l1=cl1+l1x;
-        var l2=cl2+l2x;
-        var i1=ci1+i1x;
-        var m1=cm1+m1x;
-        var hp=chp+hpx;
+    if (do1 >= line){
+        g01=Number(localStorage.getItem("g01"));
+        g02=Number(localStorage.getItem("g02"));
+        g03=Number(localStorage.getItem("g03"));
+        j01=Number(localStorage.getItem("j01"));
+        l01=Number(localStorage.getItem("l01"));
+        l02=Number(localStorage.getItem("l02"));
+        i01=Number(localStorage.getItem("i01"));
+        m01=Number(localStorage.getItem("m01"));
+        g11=parseInt(Number(localStorage.getItem("g11"))/5);
+        g21=parseInt(Number(localStorage.getItem("g21"))/5);
+        g31=parseInt(Number(localStorage.getItem("g31"))/5);
+        j11=parseInt(Number(localStorage.getItem("j11"))/5);
+        l11=parseInt(Number(localStorage.getItem("l11"))/5);
+        l21=parseInt(Number(localStorage.getItem("l21"))/5);
+        i11=parseInt(Number(localStorage.getItem("i11"))/5);
+        m11=parseInt(Number(localStorage.getItem("m11"))/5);
+        var cg1=g11*(0.2+0.1*g01/(g01+1));
+        var cg2=g21*(0.3+(0.3*Math.sqrt(g01)*g02)/((Math.sqrt(g01)+1)*(g02+1)));
+        var cg3=g31*(0.3+(0.3*Math.sqrt(g01)*g03)/((Math.sqrt(g01)+1)*(g03+1)));
+        var cj1=j11*(0.2+(0.35*Math.sqrt(g01)*j01)/((Math.sqrt(g01)+1)*(j01+1)));
+        var cl1=l11*(0.3+(0.3*Math.sqrt(g01)*l01)/((Math.sqrt(g01)+1)*(l01+1)));
+        var cl2=l21*(0.2+(0.2*Math.sqrt(g01)*l02)/((Math.sqrt(g01)+1)*(l02+1)));
+        var ci1=i11*(0.1+0.1*Math.sqrt(g01)*i01/((Math.sqrt(g01)+1)*(i01+1)));
+        var cm1=m11*(0.3+0.4*Math.sqrt(g01)*m01/((Math.sqrt(g01)+1)*(m01+1)));
+        var g1=Number(localStorage.getItem("g1"))+g11*(0.2+0.1*g01/(g01+1));
+        var g2=Number(localStorage.getItem("g2"))+g21*(0.3+(0.3*Math.sqrt(g01)*g02)/((Math.sqrt(g01)+1)*(g02+1)));
+        var g3=Number(localStorage.getItem("g3"))+g31*(0.3+(0.3*Math.sqrt(g01)*g03)/((Math.sqrt(g01)+1)*(g03+1)));
+        var j1=Number(localStorage.getItem("j1"))+j11*(0.2+(0.35*Math.sqrt(g01)*j01)/((Math.sqrt(g01)+1)*(j01+1)));
+        var l1=Number(localStorage.getItem("l1"))+l11*(0.3+(0.3*Math.sqrt(g01)*l01)/((Math.sqrt(g01)+1)*(l01+1)));
+        var l2=Number(localStorage.getItem("l2"))+l21*(0.2+(0.2*Math.sqrt(g01)*l02)/((Math.sqrt(g01)+1)*(l02+1)));
+        var i1=Number(localStorage.getItem("i1"))+i11*(0.1+0.1*Math.sqrt(g01)*i01/((Math.sqrt(g01)+1)*(i01+1)));
+        var m1=Number(localStorage.getItem("m1"))+m11*(0.3+0.4*Math.sqrt(g01)*m01/((Math.sqrt(g01)+1)*(m01+1)));
+        var hp=10*(0.1+(0.1*g01)/(g01+1));
         if (g1>20+g01){
             g1=20+g01;
             if(age>50){
                 g1=g1*0.99;
             }
         }
-        if (g2>g1*7*(1+g02/(g02+g2))){
+        if (g2>g1*7*(1+g02)/(g02+g2)){
             g2=g1*7*(1+g02/(g02+g2));
         }
-        if (g3>g1*7*(1+g03/(g03+g3))){
-            g3=g1*7*(1+g03/(g03+g3));
+        if (g3>g1*7*(1+g03)/(g03+g3)){
+            g3=g1*7*(1+g03)/(g03+g3);
         }
-        if(g2+g3>g1*10){
+        if(g2+g3 > g1*10){
             g3=g3*0.9;
             g2=g2*0.95;
         }
-        if (j1>g2*3*(1+j01/(j01+j1))){
-            j1=g2*3*(j01/j01+j1);
+        if (j1>g2*3*(1+j01)/(j01+j1)){
+            j1=g2*3*(1+j01)/(j01+j1);
         }
         if (l1>100){
             l1=100;
@@ -711,17 +662,17 @@ function gobig2(){
                 l1=l1*(1-0.005*age);
             }
         }
-        if (l2>100*(1+l02/(l02+l2))){
-            l2=100*(1+l02/(l02+l2));
+        if (l2>100*(1+l02)/(l02+l2)){
+            l2=100*(1+l02)/(l02+l2);
         }
-        if (i1>180*(1+i01/(i01+i1))){
-            i1=180*(1+i01/(i01+i1));
+        if (i1>180*(1+i01)/(i01+i1)){
+            i1=180*(1+i01)/(i01+i1);
             if (age>50){
                 i1=i1*(1-0.002*age);
             }
         }
-        if (m1>100*(1+m01/(m01+m1))){
-            m1=100*(1+m01/(m01+m1));
+        if (m1>100*(1+m01)/(m01+m1)){
+            m1=100*(1+m01)/(m01+m1);
             if (age>30){
                 m1=m1*(1-0.01*age);
             }
@@ -744,19 +695,43 @@ function gobig2(){
         localStorage.setItem("m1",m1.toString());
         localStorage.setItem("m11","31");
         localStorage.setItem("do","0");
-        localStorage.setItem("age",age.toString());
-        localStorage.setItem("exp",String(age3));
+        localStorage.setItem("age",age1.toString());
+        localStorage.setItem("exp",String(line1));
         let content = '';
         content += '<div>你長大了，成果如下</div>';
-        content += '<div>身高成長了' + String((parseInt(g1*1000)-parseInt(cg1*1000))/100)+ 'cm</div>';
-        content += '<div>體重成長了' + String((parseInt((g1+g2+g3)*100)-parseInt((cg1+cg2+cg3)*100))/100) + 'kg</div>';
-        content += '<div>跳躍力成長了' + String((parseInt(j1*100)-parseInt(cj1*100))/100) + '</div>';
-        content += '<div>視力成長了' + String((parseInt(l1*100)-parseInt(cl1*100))/100) + '</div>';
-        content += '<div>幸運成長了' + String((parseInt(l2*100)-parseInt(cl2*100))/100) + '</div>';
-        content += '<div>智力成長了' + String((parseInt(i1*100)-parseInt(ci1*100))/100) + '</div>';
-        content += '<div>魅力成長了' + String((parseInt(m1*100)-parseInt(cm1*100))/100) + '</div>';
+        content += '<div>身高成長了' + String(parseInt(cg1*1000)/100)+ 'cm</div>';
+        content += '<div>體重成長了' + String(parseInt((cg1+cg2+cg3)*100)/100) + 'kg</div>';
+        content += '<div>跳躍力成長了' + String(parseInt(cj1*100)/100) + '</div>';
+        content += '<div>視力成長了' + String(parseInt(cl1*100)/100) + '</div>';
+        content += '<div>幸運成長了' + String(parseInt(cl2*100)/100) + '</div>';
+        content += '<div>智力成長了' + String(parseInt(ci1*100)/100) + '</div>';
+        content += '<div>魅力成長了' + String(parseInt(cm1*100)/100) + '</div>';
         $("#3").append(content);
     } else {
+        if(g11>=0){
+            localStorage.setItem("g11",String(g11));
+        }
+        if(g21>=0){
+            localStorage.setItem("g21",String(g21));
+        }
+        if(g31>=0){
+            localStorage.setItem("g31",String(g31));
+        }
+        if(j11>=0){
+            localStorage.setItem("j11",String(j11));
+        }
+        if(l11>=0){
+            localStorage.setItem("l11",String(l11));
+        }
+        if(l21>=0){
+            localStorage.setItem("l21",String(l21));
+        }
+        if(i11>=0){
+            localStorage.setItem("i11",String(i11));
+        }
+        if(m11>=0){
+            localStorage.setItem("m11",String(m11));
+        }
         localStorage.setItem("do",do1.toString());
     }
 }
